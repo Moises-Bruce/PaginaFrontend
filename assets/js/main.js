fb5b4062f07673106962e9d16e7b7ca801f0cf60
MoisesBruce <contato.moisesbrucearaujo@gmail.com>
2021-07-30 23:21:41 -0400

    Pagina Frontend

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+//Faz a animação de esconder e aparecer a lista do header
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', () =>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('menu-toggle','nav-menu')
